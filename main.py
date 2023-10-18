682c0f08a21378a8cae0d431befae3fbe1d42722
pravboi <pravachanpatra@gmail.com>
2023-10-18 17:44:47 -0500

    refactor structure

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Table:
         self.guests = []
 
     def show(self):
-        print("Table: "+str(self.id) +": Major: "+self.major+ ": Seats: "+str(self.max_seats))
+        print("TABLE-"+str(self.id) +": Major: "+self.major+ ": Seats: "+str(self.max_seats))
         for g in self.guests:
             print(g.name +": "+g.major+": Type: "+g.person)
         print("-----")
@@ -29,36 +29,36 @@ class Table:
 
 class SeatingChart:
 
-    def __init__(self, event_name, num_tables, max_constants):
+    def __init__(self, event_name, num_tables, major_tables ,max_constants):
         self.event_name = event_name
         self.num_tables = num_tables
         self.tables = []
-        self.majors = ["CS","BIO"]
+        self.unqiue_majors = ["CS","BIO"]
         self.initalize_tables()
-
         self.max_constants = max_constants
 
     def initalize_tables(self):
         for i in range(self.num_tables):
-            t = Table(i+1, self.majors[i], 3)
+            t = Table(i+1, self.unqiue_majors[i], 3)
             self.tables.append(t)
     def show(self):
         for t in self.tables:
             t.show()
 
-
     def assign_guest(self, new_guest):
         for table in self.tables:
             if table.major == new_guest.major:
-
                 if table.get_type(new_guest.person) < self.max_constants[new_guest.person] and len(table.guests) < table.max_seats:
                     table.guests.append(new_guest)
                     break
 
-chart = SeatingChart("Womens Engineering", 2, {"student":1, "prof":1, "mentor":1})
+major_tables = {"Aero/Mech":3,"Archi/Civil":3,"Elec":1,"Indus":1,"Env":1,"Bio":4,"Chem":3,"CS":3,"Deans/Chairmen/IndustryFriends":2}
+
+max_constants = {"student":1, "prof":1, "mentor":1}
+chart = SeatingChart("Womens Engineering", 2, major_tables,max_constants)
 
 g1 = Guest("Bob","CS","student")
-g2 = Guest("Sam","CS","mentor")
+g2 = Guest("Sam","BIO","student")
 chart.assign_guest(g1)
 chart.assign_guest(g2)
 chart.show()
@@ -69,5 +69,14 @@ chart.show()
 
 """
 - Simulate randomness of of how guests arrive
+- A student major table is full add them to table with most similar score. Overflow Error. 
 
+TODO:
+- Create different number of tables per major
+- Number of tables = 
+- Number of seats per = 8
+- Number of students per = 
+- Number of prof per = 
+- Number of mentor per = 
+- All tables = []
 """
